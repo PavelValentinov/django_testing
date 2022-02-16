4131498d8e1c35bb0580c590e9045c328f82048b
Pavel <81234999+PavelValentinov@users.noreply.github.com>
2022-02-16 23:15:49 +0300

    Update settings.py

--- django_testing/settings.py
+++ django_testing/settings.py
@@ -80,9 +80,9 @@ WSGI_APPLICATION = 'django_testing.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'django_testing',
-        'USER': 'postgres',
-        'PASSWORD': '456124',
+        'NAME': 'name',
+        'USER': 'user',
+        'PASSWORD': 'pass',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
